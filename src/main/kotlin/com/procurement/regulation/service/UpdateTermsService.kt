5eb5082be9de024048dc41efb1f742a0e17cda78
andreykolchev <andreykolchev@gmail.com>
2018-11-20 13:35:28 +0200

    docker image

--- src/main/kotlin/com/procurement/regulation/service/UpdateTermsService.kt
+++ src/main/kotlin/com/procurement/regulation/service/UpdateTermsService.kt
@@ -3,25 +3,20 @@ package com.procurement.regulation.service
 import com.procurement.regulation.dao.TermsDao
 import com.procurement.regulation.exception.ErrorException
 import com.procurement.regulation.exception.ErrorType
-import com.procurement.regulation.model.dto.GetTermsRq
-import com.procurement.regulation.model.dto.GetTermsRs
 import com.procurement.regulation.model.dto.UpdateTermsRq
 import com.procurement.regulation.model.dto.UpdateTermsRs
 import com.procurement.regulation.model.dto.bpe.CommandMessage
 import com.procurement.regulation.model.dto.bpe.ResponseDto
-import com.procurement.regulation.model.dto.bpe.templates.AgreedMetric
 import com.procurement.regulation.model.dto.bpe.templates.ContractTerm
-import com.procurement.regulation.model.entity.TermsEntity
 import com.procurement.regulation.utils.toJson
 import com.procurement.regulation.utils.toObject
 import org.springframework.stereotype.Service
-import java.util.*
 
 @Service
 class UpdateTermsService(private val templateService: TemplateService,
                          private val termsDao: TermsDao) {
 
-      fun updateTerms(cm: CommandMessage): ResponseDto {
+    fun updateTerms(cm: CommandMessage): ResponseDto {
         val ocid = cm.context.ocid ?: throw ErrorException(ErrorType.CONTEXT)
         val dto = toObject(UpdateTermsRq::class.java, cm.data)
 
@@ -31,13 +26,19 @@ class UpdateTermsService(private val templateService: TemplateService,
         val agreedMetricsDb = contractTerm.agreedMetrics
         val agreedMetricsRqIds = agreedMetricsRq.asSequence().map { it.id }.toSet()
         val agreedMetricsDbIds = agreedMetricsDb.asSequence().map { it.id }.toSet()
-        if (!agreedMetricsDbIds.containsAll(agreedMetricsRqIds)) throw ErrorException(ErrorType.INVALID_METRIC_ID)
+        if (!agreedMetricsDbIds.containsAll(agreedMetricsRqIds))
+            throw ErrorException(ErrorType.INVALID_METRIC_ID, (agreedMetricsDbIds - agreedMetricsRqIds).toString())
         for (agreedMetricRq in agreedMetricsRq) {
             for (agreedMetricDb in agreedMetricsDb) {
                 if (agreedMetricDb.id == agreedMetricRq.id) {
-                    for (observation in agreedMetricDb.observations) {
-                        val measureRq = agreedMetricRq.observations.asSequence().firstOrNull { it.id == observation.id }?.measure
-                        observation.measure = measureRq
+                    for (observationDb in agreedMetricDb.observations) {
+                        for (observationRq in agreedMetricRq.observations) {
+                            if (observationRq.id == observationDb.id) {
+                                observationDb.measure = observationRq.measure
+                            } else {
+                                throw ErrorException(ErrorType.INVALID_OBSERVATION_ID, observationRq.id)
+                            }
+                        }
                     }
                 }
             }
