c53b3c2bb8b7ea18b602ea61323a69ca748a7424
andreykolchev <andreykolchev@gmail.com>
2018-11-20 13:46:17 +0200

    observation

--- src/main/kotlin/com/procurement/regulation/service/UpdateTermsService.kt
+++ src/main/kotlin/com/procurement/regulation/service/UpdateTermsService.kt
@@ -27,16 +27,17 @@ class UpdateTermsService(private val templateService: TemplateService,
         val agreedMetricsRqIds = agreedMetricsRq.asSequence().map { it.id }.toSet()
         val agreedMetricsDbIds = agreedMetricsDb.asSequence().map { it.id }.toSet()
         if (!agreedMetricsDbIds.containsAll(agreedMetricsRqIds))
-            throw ErrorException(ErrorType.INVALID_METRIC_ID, (agreedMetricsDbIds - agreedMetricsRqIds).toString())
+            throw ErrorException(ErrorType.INVALID_METRIC_ID, (agreedMetricsRqIds - agreedMetricsDbIds).toString())
+        val observationRqIds = agreedMetricsRq.asSequence().flatMap { it.observations.asSequence() }.map { it.id }.toSet()
+        val observationDbIds = agreedMetricsDb.asSequence().flatMap { it.observations.asSequence() }.map { it.id }.toSet()
+        if (!observationDbIds.containsAll(observationRqIds))
+            throw ErrorException(ErrorType.INVALID_OBSERVATION_ID, (observationRqIds - observationDbIds).toString())
         agreedMetricsRq.forEach { agreedMetricRq ->
             agreedMetricsDb.forEach { agreedMetricDb ->
                 if (agreedMetricDb.id == agreedMetricRq.id)
                     agreedMetricDb.observations.forEach { observationDb ->
                         agreedMetricRq.observations.forEach { observationRq ->
-                            when {
-                                observationRq.id == observationDb.id -> observationDb.measure = observationRq.measure
-                                else -> throw ErrorException(ErrorType.INVALID_OBSERVATION_ID, observationRq.id)
-                            }
+                            if (observationRq.id == observationDb.id) observationDb.measure = observationRq.measure
                         }
                     }
             }
